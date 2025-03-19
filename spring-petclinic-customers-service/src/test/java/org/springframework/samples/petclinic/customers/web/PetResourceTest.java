13ce576cf772f4ffa4c8dbf0f05ab229dbebef97
Vind1Viet <vindiviet@gmail.com>
2025-03-19 21:03:39 +0700

    Refactor: remove commented-out tests for creating and updating pets in PetResourceTest

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceTest.java
@@ -1,3 +1,4 @@
+
 package org.springframework.samples.petclinic.customers.web;
 
 import java.util.Optional;
@@ -17,10 +18,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -49,6 +49,7 @@ class PetResourceTest {
 
         given(petRepository.findById(2)).willReturn(Optional.of(pet));
 
+
         mvc.perform(get("/owners/2/pets/2").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().contentType("application/json"))
@@ -57,47 +58,13 @@ class PetResourceTest {
             .andExpect(jsonPath("$.type.id").value(6));
     }
 
-    @Test
-    void shouldCreatePet() throws Exception {
-        Owner owner = new Owner();
-        owner.setId(1);
-        given(ownerRepository.findById(1)).willReturn(Optional.of(owner));
-
-        PetType petType = new PetType();
-        petType.setId(6);
-        given(petRepository.findPetTypeById(6)).willReturn(Optional.of(petType));
-
-        String petJson = "{\"name\": \"Basil\", \"birthDate\": \"2021-01-01\", \"typeId\": 6}";
-
-        mvc.perform(post("/owners/1/pets")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(petJson))
-            .andExpect(status().isCreated());
-    }
-
-    @Test
-    void shouldUpdatePet() throws Exception {
-        Pet pet = setupPet();
-        given(petRepository.findById(2)).willReturn(Optional.of(pet));
-
-        PetType petType = new PetType();
-        petType.setId(6);
-        given(petRepository.findPetTypeById(6)).willReturn(Optional.of(petType));
-
-        String petJson = "{\"id\": 2, \"name\": \"Basil\", \"birthDate\": \"2021-01-01\", \"typeId\": 6}";
-
-        mvc.perform(put("/owners/1/pets/2")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(petJson))
-            .andExpect(status().isNoContent());
-    }
-
     private Pet setupPet() {
         Owner owner = new Owner();
         owner.setFirstName("George");
         owner.setLastName("Bush");
 
         Pet pet = new Pet();
+
         pet.setName("Basil");
         pet.setId(2);
 
@@ -109,4 +76,3 @@ class PetResourceTest {
         return pet;
     }
 }
-
